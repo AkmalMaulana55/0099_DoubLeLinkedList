d2849660038ed1880574a2ebe2fe4b8b5fd9051a
AkmalMaulana55 <vylingsmall55@gmail.com>
2025-05-15 18:42:37 +0700

    void addnode

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -20,4 +20,14 @@ public:
     {
         START = NULL;
     }
+
+    void addNode()
+    {
+        int nim;
+        string nm;
+        cout << "\nEnter the roll number of the student: ";
+        cin >> nim;
+
+        
+    }
 }
