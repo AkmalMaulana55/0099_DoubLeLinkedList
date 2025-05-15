03edca657dd55efc7b81760f7e1659693809c999
AkmalMaulana55 <vylingsmall55@gmail.com>
2025-05-15 19:11:44 +0700

    menambahkan void traverse

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -112,4 +112,14 @@ public:
         delete current;
         cout << "Record with roll number " << rollNo << " deleted" << endl;
     }
+
+    void traverse()
+    {
+        if (START == NULL)
+        {
+            cout << "\nList is empty" << endl;
+            return;
+        }
+        
+    }
 };
